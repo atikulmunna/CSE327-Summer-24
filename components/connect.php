cfcc68dc99e2beb6ed873045b2f124ed7744d280
chishti055nsu <106840167+chishti055nsu@users.noreply.github.com>
2024-11-30 19:28:22 +0600

    SINGLETON_connect.php
    It helps us to connect to database more efficiently .

--- components/connect.php
+++ components/connect.php
@@ -1,4 +1,35 @@
-<?php
+class DataBase
+{
+    private static $instance;
+    private $connection;
+
+    private function __construct()
+    {
+        $this->connection = mysqli_connect("localhost", "username", "password", "database");
+    }
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
+
+// Use of it :
+$db = Database::getInstance();
+$connection = $db->getConnection();
+
+
+
+/*<?php
 
 $host = 'localhost';
 $db = 'plantverse';
@@ -10,4 +41,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-?>
+?>*/
