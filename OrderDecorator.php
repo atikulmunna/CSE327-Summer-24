89db2cba6f4af6975962b6027a70f7440830d757
Atikul Islam Munna <103902791+atikulmunna@users.noreply.github.com>
2024-12-06 10:46:26 +0600

    Add files via upload

---
+++ OrderDecorator.php
@@ -0,0 +1,21 @@
+<?php
+abstract class OrderDecorator extends Order {
+    protected $order;
+
+    public function __construct(Order $order) {
+        $this->order = $order;
+    }
+
+    public function getTotal() {
+        return $this->order->getTotal();
+    }
+}
+
+class GiftWrapDecorator extends OrderDecorator {
+    private $giftWrapCost = 10; // Example cost for gift wrap
+
+    public function getTotal() {
+        return $this->order->getTotal() + $this->giftWrapCost;
+    }
+}
+?>
